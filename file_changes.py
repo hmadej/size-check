82979d6fb3f0c4276e2a752d06ac7e43acc055a1
Henry Madej <hmadej@protonmail.com>
2020-09-14 17:37:22 -0400

    remove debug print statements

--- file_changes.py
+++ file_changes.py
@@ -60,8 +60,3 @@ if __name__ == '__main__':
         },
         body=json.dumps(table).encode('utf-8')
     )
-    print(master_sizes)
-    print(branch_sizes)
-    print(url)
-    print(r.status)
-    print(r.data)
