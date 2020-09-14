d87a7194260ebdae280b375ede8e395c867a0cc4
Henry Madej <hmadej@protonmail.com>
2020-09-14 14:47:20 -0400

    removed env variables

--- file_changes.py
+++ file_changes.py
@@ -3,10 +3,7 @@ import subprocess
 import re
 from markdown import *
 
-
-BRANCH_name = os.environ['branch'] if 'branch' in os.environ else 'branch'
-MASTER_name = os.environ['main'] if 'main' in os.environ else 'master'
-PATTERN = re.compile(f'(\d{1,8})[^{BRANCH_name[0]}|^{MASTER_name[0]}]*({BRANCH_name}|{MASTER_name})\/(.*)')
+PATTERN = re.compile('(\d{1,8}).*(branch|main)\/(.*)')
 CLI_ARGS = ['du', '-k', '-d', '1']
 # group 1 is size as an integer, group 3 is filepath
 
@@ -22,6 +19,9 @@ def get_directory_size(path):
                 capture_output=True
             ).stdout, 'utf-8')
 
+            print(stdout)
+            for line in stdout.split('\n'):
+                print(PATTERN.match(line))
             return {
                 matches.group(3): int(matches.group(1))
                 for line in stdout.split('\n')
@@ -37,12 +37,12 @@ def get_directory_size(path):
 
 def get_directory_sizes(source_dir, list_dir):
     return {
-        fp: get_directory_size(f'./{source_dir}/{fp}')
+        fp: get_directory_size(f'../{source_dir}/{fp}')
         for fp in list_dir
     }
 
 
 if __name__ == '__main__':
-    master_sizes = get_directory_sizes(MASTER_name, sys.argv[1:])
-    branch_sizes = get_directory_sizes(BRANCH_name, sys.argv[1:])
+    master_sizes = get_directory_sizes('main', sys.argv[1:])
+    branch_sizes = get_directory_sizes('branch', sys.argv[1:])
     print(make_table(master_sizes, branch_sizes))
