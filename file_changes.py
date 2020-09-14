2b4ed522e35c6cf0fd071cf2ced5c4c8ea0f7de1
Henry Madej <hmadej@protonmail.com>
2020-09-14 12:48:59 -0400

    added script for comparing build sizes

---
+++ file_changes.py
@@ -0,0 +1,49 @@
+import os, sys
+import subprocess
+import re
+from markdown import *
+
+CLI_ARGS = ['du', '-k', '-d', '1']
+# group 1 is size as an integer, group 3 is filepath
+PATTERN = re.compile('(\d{1,8})\s*(master|branch)\/(.*)')
+
+'''
+Run cli tool DU to get directory size information
+convert stdout string information into a dict
+'''
+def get_directory_size(path):
+    try:
+        if os.path.exists(path):
+            stdout = str(subprocess.run(
+                [*CLI_ARGS, path],
+                capture_output=True
+            ).stdout, 'utf-8')
+
+
+            return {
+                matches.group(3): int(matches.group(1))
+                for line in stdout.split('\n')
+                if (matches := PATTERN.match(line))
+            }
+        else:
+            print(f'No {path} exists')
+            return {}
+    except Exception as e:
+        print(e)
+        return {}
+
+
+def get_directory_sizes(source_dir, list_dir):
+    return {
+        fp: get_directory_size(f'{source_dir}/{fp}')
+        for fp in list_dir
+    }
+
+
+if __name__ == '__main__':
+    branch_name = os.environ['branch'] if 'branch' in os.environ else 'branch'
+    master_name = os.environ['main'] if 'master' in os.environ else 'master'
+    master_sizes = get_directory_sizes(master_name, sys.argv[1:])
+    branch_sizes = get_directory_sizes(branch_name, sys.argv[1:])
+    print(make_table(master_sizes, branch_sizes))
+    
---
+++ markdown.py
@@ -0,0 +1,38 @@
+
+def format_diff(master, branch):
+    if master == branch:
+        return ':heavy_check_mark:'
+    elif master < branch:
+        return f':warning: +{format_size(branch - master)}'
+    else:
+        return f':white_check_mark: {format_size(branch - master)}'
+
+
+def format_size(number):
+    if number == 0:
+        return '-'
+    if number // 1024 == 0:
+        return f'{number}kB'
+    else:
+        return f'{number/1024:.1f}MB'
+
+
+def make_table(master, branch):
+    table = ''
+    for key, value in master.items():
+        if type(value) is dict:
+            table += '|   | master | branch | change |\n| --- | --- | --- | --- |\n'
+            table += make_table(value, branch[key])
+        else:
+            if key in branch:
+                table += table_row(key, value, branch[key])
+            else:
+                table += table_row(key, value, 0)
+    if (keys := set(branch) - set(master)):
+        for key in keys:
+            table += table_row(key, 0, branch[key])
+    return table + '\n'
+
+
+def table_row(key, v1, v2):
+    return f'| {key} | {format_size(v1)} | {format_size(v2)} | {format_diff(v1, v2)} |\n'
